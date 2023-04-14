75253723b6a7056c2b90302bf79196a3fb561fc9
Nicolas Drygas <nicolas.drygas@live.com>
2023-04-13 18:39:32 -0700

    Initial commit

---
+++ version2/raffle.py
@@ -0,0 +1,8 @@
+"""Randomly pick customer and print customer info"""
+
+from random import choice
+from 
+# Add code starting here
+
+# Hint: remember to import any functions you need from
+# other files or libraries
